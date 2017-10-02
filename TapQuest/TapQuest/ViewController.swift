43af5bc44a5c8c65622e642a1b95fa267672e25f
= <johan.sogaard@verisure.com>
2017-10-02 18:40:37 +0200

    Created xcproject

---
+++ TapQuest/TapQuest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapQuest
+//
+//  Created by Johan Sögaard on 2017-10-02.
+//  Copyright © 2017 Verisure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
